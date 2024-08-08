d94b8d9b64bd9594a55a2f8d14dfa6b96f896995
Dominic <andro5@web.de>
2024-08-08 11:24:38 +0200

    add csrf testing

---
+++ backend/src/test/java/com/example/backend/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @WithMockUser
+    public void test() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/api/hello")
+                .content("Test")
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
